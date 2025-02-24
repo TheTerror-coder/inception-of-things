f77d1f15194083b66b2aafd0108799bb45bd0c39
theterror-coder <jfaye@student.42lyon.fr>
2025-02-24 19:04:21 +0100

    p1 clean

--- p1/tools/worker-node-script.sh
+++ p1/tools/worker-node-script.sh
@@ -1,11 +1,6 @@
 #!/bin/sh -e
 
 echo SERVER_NODE_HOME: $SERVER_NODE_HOME
-# sudo mkdir -p /etc/rancher/k3s/
-# sudo mv /tmp/config.yaml $K3S_CONFIG_FILE
-# echo "export K3S_CONFIG_FILE=$K3S_CONFIG_FILE" >> $SERVER_NODE_HOME/.bashrc
 export INSTALL_K3S_EXEC="agent --server $K3S_URL --node-ip $WORKER_NODE_ETH1_IP"
-# echo "export INSTALL_K3S_EXEC='$INSTALL_K3S_EXEC'" >> $SERVER_NODE_HOME/.bashrc
-# echo "export KUBECONFIG='/etc/rancher/k3s/k3s.yaml'" >> $SERVER_NODE_HOME/.bashrc
 sudo apt update && sudo apt install -y curl
 curl -sfL https://get.k3s.io | sh -s -
