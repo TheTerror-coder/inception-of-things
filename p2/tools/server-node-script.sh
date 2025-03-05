8280dcd971fc3295b4b06a7e5a6610f3532182e5
theterror-coder <jfaye@student.42lyon.fr>
2025-03-06 00:40:25 +0100

    feat: added a customized image on a custom docker registry and deployed it in my kubernetes pod

--- p2/tools/server-node-script.sh
+++ p2/tools/server-node-script.sh
@@ -5,6 +5,7 @@ export INSTALL_K3S_EXEC="server --debug --log '/tmp/k3s.log' --cluster-init --fl
 cat $SERVER_NODE_HOME/.bashrc | grep --quiet 'export SERVER_NODE_HOME' || echo "export SERVER_NODE_HOME=$SERVER_NODE_HOME" >> $SERVER_NODE_HOME/.bashrc
 cat $SERVER_NODE_HOME/.bashrc | grep --quiet 'export KUBECONFIG' || echo "export KUBECONFIG='/etc/rancher/k3s/k3s.yaml'" >> $SERVER_NODE_HOME/.bashrc
 cat $SERVER_NODE_HOME/.bashrc | grep --quiet 'source <(kubectl completion bash)' || echo "source <(kubectl completion bash)" >> $SERVER_NODE_HOME/.bashrc
+cat $SERVER_NODE_HOME/.bashrc | grep --quiet "alias k='kubectl'" || echo "alias k='kubectl'" >> $SERVER_NODE_HOME/.bashrc
 sudo apt update && sudo apt install -y curl vim
 curl -sfL https://get.k3s.io | sh -
 until kubectl version 2> /dev/null | grep --no-messages 'Server Version: v'; do sleep 1; done
